4a55f3d0f8b5592a8a63351070b81920cac4eca4
patrikmojzis <38838813+patrikmojzis@users.noreply.github.com>
2025-11-21 14:51:29 +0100

    update default model to gpt5.1

--- fast_agents/agent.py
+++ fast_agents/agent.py
@@ -2,16 +2,17 @@ from __future__ import annotations
 
 from typing import Optional, Type
 
-from pydantic import BaseModel, Field, ConfigDict
 from openai.types import ReasoningEffort
+from pydantic import BaseModel, Field, ConfigDict
 
-from fast_agents.tool import Tool
 from fast_agents.helpers.handoffs_helper import HandoffTool
+from fast_agents.tool import Tool
+
 
 class Agent(BaseModel):
     name: str = Field("agent")
     instructions: str = Field("You are a helpful assistant.")
-    model: str = Field("gpt-5", description="Default model to use.")
+    model: str = Field("gpt-5.1", description="Default model to use.")
     tools: list[Tool | dict] = Field([], description="List of tools available. Can be a list of Tool objects or a list of native tool like code_interpreter, web_search_preview, image_generation.")
     output_type: Optional[Type[BaseModel]] = Field(None, description="Structured output.")
     temperature: Optional[float] = Field(None, description="Value between 0 and 2. Reasoning models don't support temperature.")
